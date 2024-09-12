88c94022ced4f132a02143e4a5119d51594e54f3
Asif-karim007 <asif0171797@gmail.com>
2024-09-12 16:45:19 +0600

    headless

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
+import threading
+import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from login_interaction import LoginInteraction
-import threading
+from selenium.webdriver.chrome.options import Options
 import time
+from login_interaction import LoginInteraction 
 
+# def simulate_user(user_id):
+#     driver = webdriver.Chrome(service=Service())
+#     login_interaction = LoginInteraction(driver)
+
+#     username = "superadmin"
+#     password = "n123"
+
+#     login_interaction.login(username, password)
+#     time.sleep(2)
+#     login_interaction.closeModal(username)
+#     login_interaction.openProspect(username)
+#     login_interaction.createProspect(f"User{user_id}")
+
+#     driver.quit()
+#     print(f"Finished session and closed browser for user {user_id}.")
 def simulate_user(user_id):
-    driver = webdriver.Chrome(service=Service())
+    logging.getLogger("selenium").setLevel(logging.WARNING)
+    service_obj = Service()
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run in headless mode
+    chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration
+    chrome_options.add_argument("--window-size=1200x800")  # Set a window size
+
+    driver = webdriver.Chrome(service=service_obj, options=chrome_options)
+
     login_interaction = LoginInteraction(driver)
 
     username = "superadmin"
-    password = "n123"
+    password = "n123"  
 
     login_interaction.login(username, password)
+
     time.sleep(2)
     login_interaction.closeModal(username)
+
     login_interaction.openProspect(username)
-    login_interaction.createProspect(f"User{user_id}")
+
+    login_interaction.createProspect(username)
 
     driver.quit()
-    print(f"Finished session and closed browser for user {user_id}.")
-
+    print(f"Finished session and closed browser for User {user_id}.")
+    
+    
 def simulate_concurrent_users(num_users): 
     threads = []
     for user_id in range(1, num_users + 1):
@@ -35,5 +65,5 @@ def simulate_concurrent_users(num_users):
 
 # Main execution
 if __name__ == "__main__":
-    num_users = 10
+    num_users = 30
     simulate_concurrent_users(num_users)
