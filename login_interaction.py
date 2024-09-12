250e7a3edd850f4b3834ea7fd32c05858b9f0893
Asif-karim007 <asif0171797@gmail.com>
2024-09-12 15:08:51 +0600

    for windows use

--- login_interaction.py
+++ login_interaction.py
@@ -1,13 +1,170 @@
+# import time
+# import re
+# import random
+# import sys
+# import select
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.common.exceptions import ElementClickInterceptedException
+
+
+# class LoginInteraction:
+#     def __init__(self, driver):
+#         self.driver = driver
+#         self.driver.maximize_window()
+#         self.driver.implicitly_wait(10)
+    
+#     def login(self, username, password):
+#         try:
+#             self.driver.get("https://qa.salebee.net/")
+#             self.driver.find_element(By.XPATH, "//*[@id='UserName']").send_keys(username)
+#             self.driver.find_element(By.XPATH, "//*[@id='Password']").send_keys(password)
+#             self.driver.find_element(By.XPATH, "//*[@id='myModal']/div/div/div[2]/form/div[3]/input").click()
+#             current_url = self.driver.current_url
+            
+#             if current_url == "https://qa.salebee.net/Employee/MyProfile":
+#                 print(f"User {username}: Login successful!")
+#             else:
+#                 print(f"User {username}: Login failed.")
+                
+#         except Exception as e:
+#             print(f"User {username}: Error occurred during login: {e}")
+    
+#     def closeModal(self, username):
+#         try:
+#             modal = WebDriverWait(self.driver, 10).until(
+#                 EC.visibility_of_element_located((By.XPATH, "//div[@class='modal-header']"))
+#             )
+#             print("Modal is visible")
+
+#             close_button = modal.find_element(By.XPATH, "//*[@id='taskNotification']/div/div/div[1]/button/span/i")
+#             close_button.click()
+#             time.sleep(2)
+#             print("Closed the modal")
+
+#         except Exception as e:
+#             print(f"User {username}: Modal not found: {e}")
+            
+#     def extract_number(self, text):
+#         match = re.search(r'\d+', text)
+#         if match:
+#             return int(match.group())
+#         return 0
+
+#     def openProspect(self, username):
+#         try:
+#             self.driver.find_element(By.XPATH, "//*[@id='sidebarmenu']/div[2]/div/ul/li[6]").click()
+#             WebDriverWait(self.driver, 10).until(
+#                 EC.visibility_of_element_located((By.XPATH, "//*[@id='prospectGrid']"))
+#             )
+#             print(f"Prospect table has been loaded")
+            
+#             try:
+#                 current_prospect_element = WebDriverWait(self.driver, 20).until(
+#                     EC.presence_of_element_located((By.XPATH, '//*[@id="prospectstagegroup"]/div[1]'))
+#                 )
+#                 current_prospect_text = current_prospect_element.text
+#                 self.current_prospect_number = self.extract_number(current_prospect_text)
+#                 print(f"Total prospect number: {self.current_prospect_number}")
+                
+#             except Exception as e:
+#                 print(f"Error occurred while extracting number: {e}")
+                
+#         except Exception as e:
+#             print(f"Can not open prospect table: {e}")
+            
+
+#     def createProspect(self, username):    
+#         global total_prospects_created  # Use the global counter
+#         counter = 0
+#         created_prospects = []  # List to store created prospect names
+#         print(f"User {username}: Press Enter to stop the process...\n")
+        
+#         while True: 
+#             if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+#                 input()
+#                 print(f"User {username}: Stopping the process...")
+#                 break
+            
+#             random_suffix = random.randint(10000, 99999)
+#             prospect_name = f"Automated Testing #{random_suffix}"
+            
+#             try:
+#                 add_prospect_button = self.driver.find_element(By.XPATH, "//*[@id='ChildContainer']/div[1]/div[1]/div[1]/div[1]")
+                
+#                 # Wait until the element is clickable
+#                 WebDriverWait(self.driver, 20).until(
+#                     EC.element_to_be_clickable((By.XPATH, "//*[@id='ChildContainer']/div[1]/div[1]/div[1]/div[1]"))
+#                 )
+                
+#                 # Scroll to the element and attempt the click
+#                 actions = ActionChains(self.driver)
+#                 actions.move_to_element(add_prospect_button).perform()
+#                 add_prospect_button.click()
+#                 time.sleep(2)
+#                 self.driver.find_element(By.XPATH, "//*[@id='org-Name']").send_keys(prospect_name)
+#                 self.driver.find_element(By.XPATH, "//*[@id='myModal']/div/div/div[3]/div[1]/button").click()
+#                 time.sleep(2)
+                
+#                 WebDriverWait(self.driver, 20).until(
+#                     EC.invisibility_of_element_located((By.XPATH, "/html/body/div[31]/div[4]"))
+#                 )
+
+#                 # Refresh the prospect table
+#                 refresh_button = WebDriverWait(self.driver, 20).until(
+#                     EC.visibility_of_element_located((By.XPATH, "//*[@id='ChildContainer']/div[1]/div[1]/div[1]/div[2]/a/i"))
+#                 )
+#                 refresh_button.click()
+#                 time.sleep(2)
+
+#                 WebDriverWait(self.driver, 40).until(
+#                     EC.visibility_of_element_located((By.XPATH, "//*[@id='prospectGrid']"))
+#                 )
+
+#                 # Get the new prospect number
+#                 try:
+#                     new_prospect_element = WebDriverWait(self.driver, 20).until(
+#                         EC.presence_of_element_located((By.XPATH, '//*[@id="prospectstagegroup"]/div[1]'))
+#                     )
+#                     new_prospect_text = new_prospect_element.text
+#                     new_prospect_number = self.extract_number(new_prospect_text)
+#                     created_prospects.append(prospect_name)  # Store the name of the created prospect
+#                     print(f"User {username}: Total prospect number after creating a new prospect: {new_prospect_number}")
+#                     counter += 1 
+#                     print(f"User {username}: Prospect {counter} created: {prospect_name}")
+#                 except Exception as e:
+#                     print(f"User {username}: Error occurred while counting new prospect total: {e}")
+                
+#                 time.sleep(2)
+
+#                 # Compare prospect numbers
+#                 if new_prospect_number > self.current_prospect_number:
+#                     print(f"User {username}: Successfully created a new prospect!")
+#                 else:
+#                     print(f"User {username}: Failed to create a new prospect.")
+            
+#             except ElementClickInterceptedException as e:
+#                 print(f"User {username}: Element click intercepted: {e}. Trying again after waiting...")
+#                 time.sleep(2)  
+                
+#             except Exception as e:
+#                 print(f"User {username}: Error occurred while creating new prospect: {e}")
+
+#         print(f"User {username}: Total number of prospects created automatically: {counter}")
+#         print(f"User {username}: All created prospect names: {created_prospects}")
+
 import time
 import re
 import random
 import sys
-import select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementClickInterceptedException  # Correct import
+
 
 
 class LoginInteraction:
@@ -76,26 +233,26 @@ class LoginInteraction:
             print(f"Can not open prospect table: {e}")
             
 
-    def createProspect(self, username):    
+    def createProspect(self, username):
         global total_prospects_created  # Use the global counter
         counter = 0
         created_prospects = []  # List to store created prospect names
-        print(f"User {username}: Press Enter to stop the process...\n")
-        
+
+        print(f"User {username}: Starting automated prospect creation...\n")
         while True: 
-            if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-                input()
-                print(f"User {username}: Stopping the process...")
+            # Check for a stop condition, like max number of prospects created
+            if counter >= 5:  # Example: Stop after 5 prospects
+                print(f"User {username}: Stopping the process after creating {counter} prospects.")
                 break
             
-            random_suffix = random.randint(10000, 99999)
+            random_suffix = random.randint(1000000, 9999999)
             prospect_name = f"Automated Testing #{random_suffix}"
             
             try:
                 add_prospect_button = self.driver.find_element(By.XPATH, "//*[@id='ChildContainer']/div[1]/div[1]/div[1]/div[1]")
                 
                 # Wait until the element is clickable
-                WebDriverWait(self.driver, 20).until(
+                WebDriverWait(self.driver, 40).until(
                     EC.element_to_be_clickable((By.XPATH, "//*[@id='ChildContainer']/div[1]/div[1]/div[1]/div[1]"))
                 )
                 
@@ -103,17 +260,17 @@ class LoginInteraction:
                 actions = ActionChains(self.driver)
                 actions.move_to_element(add_prospect_button).perform()
                 add_prospect_button.click()
-                time.sleep(2)
+                time.sleep(4)
                 self.driver.find_element(By.XPATH, "//*[@id='org-Name']").send_keys(prospect_name)
                 self.driver.find_element(By.XPATH, "//*[@id='myModal']/div/div/div[3]/div[1]/button").click()
-                time.sleep(2)
+                time.sleep(4)
                 
-                WebDriverWait(self.driver, 20).until(
+                WebDriverWait(self.driver, 40).until(
                     EC.invisibility_of_element_located((By.XPATH, "/html/body/div[31]/div[4]"))
                 )
 
                 # Refresh the prospect table
-                refresh_button = WebDriverWait(self.driver, 20).until(
+                refresh_button = WebDriverWait(self.driver, 40).until(
                     EC.visibility_of_element_located((By.XPATH, "//*[@id='ChildContainer']/div[1]/div[1]/div[1]/div[2]/a/i"))
                 )
                 refresh_button.click()
@@ -125,7 +282,7 @@ class LoginInteraction:
 
                 # Get the new prospect number
                 try:
-                    new_prospect_element = WebDriverWait(self.driver, 20).until(
+                    new_prospect_element = WebDriverWait(self.driver, 40).until(
                         EC.presence_of_element_located((By.XPATH, '//*[@id="prospectstagegroup"]/div[1]'))
                     )
                     new_prospect_text = new_prospect_element.text
@@ -137,7 +294,7 @@ class LoginInteraction:
                 except Exception as e:
                     print(f"User {username}: Error occurred while counting new prospect total: {e}")
                 
-                time.sleep(2)
+                time.sleep(4)
 
                 # Compare prospect numbers
                 if new_prospect_number > self.current_prospect_number:
@@ -147,7 +304,7 @@ class LoginInteraction:
             
             except ElementClickInterceptedException as e:
                 print(f"User {username}: Element click intercepted: {e}. Trying again after waiting...")
-                time.sleep(2)  
+                time.sleep(4)  
                 
             except Exception as e:
                 print(f"User {username}: Error occurred while creating new prospect: {e}")
--- main.py
+++ main.py
@@ -1,3 +1,43 @@
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from login_interaction import LoginInteraction
+# import threading
+# import time
+
+# def simulate_user(user_id):
+#     driver = webdriver.Chrome(service=Service())
+#     login_interaction = LoginInteraction(driver)
+
+#     username = "superadmin"
+#     password = "n123"
+
+#     login_interaction.login(username, password)
+#     time.sleep(2)
+#     login_interaction.closeModal(username)
+#     login_interaction.openProspect(username)
+#     login_interaction.createProspect(f"User{user_id}")
+
+#     driver.quit()
+#     print(f"Finished session and closed browser for user {user_id}.")
+
+# def simulate_concurrent_users(num_users): 
+#     threads = []
+#     for user_id in range(1, num_users + 1):
+#         thread = threading.Thread(target=simulate_user, args=(user_id,))
+#         threads.append(thread)
+#         thread.start()
+
+#     for thread in threads:
+#         thread.join()
+        
+
+
+
+# # Main execution
+# if __name__ == "__main__":
+#     num_users = 5 
+#     simulate_concurrent_users(num_users)
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from login_interaction import LoginInteraction
@@ -7,7 +47,6 @@ import time
 def simulate_user(user_id):
     driver = webdriver.Chrome(service=Service())
     login_interaction = LoginInteraction(driver)
-
     username = "superadmin"
     password = "n123"
 
@@ -35,5 +74,5 @@ def simulate_concurrent_users(num_users):
 
 # Main execution
 if __name__ == "__main__":
-    num_users = 5 
+    num_users = 5
     simulate_concurrent_users(num_users)
